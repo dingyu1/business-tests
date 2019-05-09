d7872f6ac413e4b841250d3678ae8e5f24ad7e32
“minshuai” <1599075576@qq.com>
2019-05-09 14:10:41 +0800

    add file

--- auto-test/nic/onboard/nic_basic_function/nic_basic_Negotiation/onboard_NIC_BASIC_Negotiation_001.sh
+++ auto-test/nic/onboard/nic_basic_function/nic_basic_Negotiation/onboard_NIC_BASIC_Negotiation_001.sh
@@ -20,12 +20,13 @@
 #*****************************************************************************************
 
 #加载公共函数,具体看环境对应的位置修改
-. ../../../../utils/error_code.inc
-. ../../../../utils/test_case_common.inc
-. ../../../../utils/sys_info.sh
-. ../../../../utils/sh-test-lib
+#. ./utils/test_case_common.inc
 #. ./utils/error_code.inc
-#. ./utils/test_case_common.inc 
+. ../../../../../utils/error_code.inc
+. ../../../../../utils/test_case_common.inc
+. ../../../../../utils/sys_info.sh
+. ../../../../../utils/sh-test-lib
+. ../../../../../utils/env_parameter.inc     
  
 #获取脚本名称作为测试用例名称
 test_name=$(basename $0 | sed -e 's/\.sh//')
@@ -43,49 +44,26 @@ RESULT_FILE=${TMPDIR}/${test_name}.result
 test_result="pass"
 
 #************************************************************#
-# Name        : find_physical_card                                        #
-# Description : 查询物理网卡                                 #
-# Parameters  : 无           #
-# return value：total_network_cards[]
+# Name        : distinguish_card                               #
+# Description : 区分板载和标卡                              #
+# Parameters  : 无                                           #
 #************************************************************#
-function find_physical_card(){
-	total_network_cards=(`ls /sys/class/net/`)
-	virtual_network_cards=(`ls /sys/devices/virtual/net/`)
-	len_total=${#total_network_cards[@]}
-	len_virtual=${#virtual_network_cards[@]}
+function distinguish_card(){
+    #查找所有物理网卡
+    fn_get_physical_network_card total_network_cards
+	total_network_cards=(`echo ${total_network_cards[@]}`)
 	
-	#去除非目录文件
-	for ((i=0;i<${len_total};i++))
-	do
-		if [ ! -d "/sys/class/net/${total_network_cards[i]}" ]; then
-			unset total_network_cards[i]
-		fi	
-	done
-
-	#去除非目录文件
-	for ((i=0;i<${len_virtual};i++))
-	do
-		if [ ! -d "/sys/devices/virtual/net/${virtual_network_cards[i]}" ]; then
-			unset virtual_network_cards[i]
-		fi	
-	done
-
-	#去除虚拟网卡
-	for ((i=0;i<${len_total};i++))
-	do
-		for ((j=0;j<${len_virtual};j++))
-		do
-			if [ "${total_network_cards[i]}" == "${virtual_network_cards[j]}" ]; then
-				unset total_network_cards[i]
-			fi
-		done	
-	done
-
-	for net in ${virtual_network_cards[@]}
-	do
-		PRINT_LOG "INFO" "please check this $net port"
-	done
-	
+    for ((i=0;i<${#total_network_cards[@]};i++))
+    do
+        driver=`ethtool -i ${total_network_cards[i]} | grep driver | awk '{print $2}'`
+        if [ "$driver" == "hns" -o "$driver" == "hns3" ];then
+            board_card[i]=${total_network_cards[i]}
+            board_card=(`echo ${board_card[@]}`)
+        else
+            standard_card[i]=${total_network_cards[i]}
+            standard_card=(`echo ${standard_card[@]}`)
+        fi
+    done
 }
 #************************************************************#
 # Name        : verify_negotiate                               #
@@ -93,7 +71,7 @@ function find_physical_card(){
 # Parameters  : 无                                           #
 #************************************************************#
 function verify_negotiate(){
-	for net in ${total_network_cards[@]}
+	for net in ${board_card[@]}
 	do
 		bus_num=`ethtool -i $net | grep bus | awk '{print $2}'`
 		LnkSta=`lspci -s $bus_num -vvv | grep LnkSta: | cut -d " " -f 2`
@@ -102,10 +80,10 @@ function verify_negotiate(){
 		#网卡协商正常
 		then 
 			PRINT_LOG "INFO" "$net standard speed is equal to capacity speed."
-			fn_writeResultFile "${RESULT_FILE}" "$net speed negotiate" "pass"
+			fn_writeResultFile "${RESULT_FILE}" "${net}_speed_negotiate" "pass"
 		else
 			PRINT_LOG "FATAL" "$net standard speed is not equal to capacity speed, please check it."
-			fn_writeResultFile "${RESULT_FILE}" "$net speed negotiate" "fail"
+			fn_writeResultFile "${RESULT_FILE}" "${net}_speed_negotiate" "fail"
 			return 1
 		fi	
 	done
@@ -116,29 +94,34 @@ function verify_negotiate(){
 # Parameters  : 无                                           #
 #************************************************************#
 function verify_connect(){
-	#打开所有网口
-	for net in ${total_network_cards[@]}
+	debug=false
+	password=$env_tc_passwd
+	test_ip=($env_tc_on_board_fiber_0 $env_tc_on_board_fiber_10 $env_tc_on_board_TP_20 $env_tc_on_board_TP_30)
+	if [ "$debug" = true ];then
+		password=root
+		test_ip=(192.168.1.6 192.168.10.6 192.168.20.6 192.168.30.6)
+		env_tc_on_board_fiber_0=192.168.1.6
+	fi
+	SSH="sshpass -p $password ssh -o StrictHostKeyChecking=no"
+	
+	
+	for ip in ${test_ip[@]}
 	do
-		ip link set dev $net up
-		sleep 2
-	done
-	#查找链路正常的网口		
-	for net in ${total_network_cards[@]}
-	do
-		ip a add 10.28.26.18/24 dev $net
-		echo "$net ping test ...."
-		ping 10.28.26.18 -c 5
+		ping $ip -c 5
 		if [ $? -eq 0 ]
-		#网卡连通正常
 		then 
-			PRINT_LOG "INFO" "$net connectivity normal."
-			fn_writeResultFile "${RESULT_FILE}" "$net connectivity" "pass"
+			PRINT_LOG "INFO" "$ip connect properly."
+			fn_writeResultFile "${RESULT_FILE}" "${ip}_connect" "pass"
 		else
-			PRINT_LOG "FATAL" "$net connectivity isn't normal, please check it."
-			fn_writeResultFile "${RESULT_FILE}" "$net connectivity" "fail"
+			PRINT_LOG "FATAL" "$ip connect is not normal."
+			fn_writeResultFile "${RESULT_FILE}" "${ip}_connect" "fail"
+			ip a
+			echo "client-------------"
+			ping $env_tc_on_board_fiber_0 -c 5
+			$SSH root@$env_tc_on_board_fiber_0 "ip a"
 			return 1
 		fi
-		ip a del 10.28.26.18/24 dev $net
+		sleep 5
 	done
 }
 
@@ -154,7 +137,8 @@ function init_env()
         PRINT_LOG "WARN" " You must be root user " 
         return 1
     fi
-	find_physical_card
+	sshpass -h || fn_install_pkg "sshpass" 10
+	distinguish_card
     #自定义测试预置条件检查实现部分：比如工具安装，检查多机互联情况，执行用户身份 
       #需要安装工具，使用公共函数install_deps，用法：install_deps "${pkgs}"
       #需要日志打印，使用公共函数PRINT_LOG，用法：PRINT_LOG "INFO|WARN|FATAL" "xxx"
@@ -163,11 +147,9 @@ function init_env()
 #测试执行
 function test_case()
 {
-	for ((i=1;i<=2;i++))
-	do
-		verify_negotiate
-		verify_connect
-	done
+	verify_negotiate
+	verify_connect
+
     #检查结果文件，根据测试选项结果，有一项为fail则修改test_result值为fail，
     check_result ${RESULT_FILE}
 }
