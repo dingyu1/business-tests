cd310b0d7c1b8aa03a28424683c27cb5ddde5d82
dingyu1 <1136920311@qq.com>
2019-05-15 23:18:48 -0400

    modify install_netperf.sh by dingyu

--- auto-test/nic/onboard/nic_basic_function/nic_basic_Statistics/install_netperf.sh
+++ auto-test/nic/onboard/nic_basic_function/nic_basic_Statistics/install_netperf.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-source ../../../../../utils/sys_info.sh
-source ../../../../../utils/sh-test-lib
-
-#安装依赖包
-install_deps "wget gcc automake make sshpass"
+distro=""
+sys_info=$(cat /etc/os-release | grep PRETTY_NAME)
+if [ "$(echo $sys_info |grep -E 'UBUNTU|Ubuntu|ubuntu')"x != ""x ]; then
+    distro="ubuntu"
+elif [ "$(echo $sys_info |grep -E 'cent|CentOS|centos')"x != ""x ]; then
+    distro="centos"
+elif [ "$(echo $sys_info |grep -E 'fed|Fedora|fedora')"x != ""x ]; then
+    distro="fedora"
+elif [ "$(echo $sys_info |grep -E 'DEB|Deb|deb')"x != ""x ]; then
+    distro="debian"
+elif [ "$(echo $sys_info |grep -E 'SUSE|OpenSuse|opensuse|openSUSE')"x != ""x ]; then
+    distro="suse"
+elif [ "$(echo $sys_info|grep -E 'redhat|Red')"x != ""x ]; then
+    distro="redhat"
+else
+    distro="ubuntu"
+fi
 
 #关闭防火墙
 case "$distro" in
     centos|redhat)
+		yum install wget gcc automake make sshpass -y
 		systemctl stop firewalld
 		;;
 	ubuntu|debian)
+		apt install wget gcc g++ automake make sshpass -y
 		apt install ufw -y
 		ufw disable
 		;;
 	suse)
+		zypper install -y wget gcc gcc-c++ automake make sshpass 
 		zypper install -y firewalld
 		systemctl stop  firewalld
 		;;
